c001624269aa1b8a5a3d71f845b46989bb288b21
yellu <wkwk0120@gmail.com>
2023-07-07 14:48:26 +0900

    Test Commit (Yi)

--- app/src/main/java/edu/kaist/cs/teamfinder/MainActivity.kt
+++ app/src/main/java/edu/kaist/cs/teamfinder/MainActivity.kt
@@ -15,6 +15,7 @@ import edu.kaist.cs.teamfinder.ui.theme.TeamFinderTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Hello World
         setContent {
             TeamFinderTheme {
                 // A surface container using the 'background' color from the theme

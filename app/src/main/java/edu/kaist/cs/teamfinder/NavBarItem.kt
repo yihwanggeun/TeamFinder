56377e8f15dcfe6b46fd16d08ab8ce1fbf928098
Mo Jihun <86469551+Morivy42@users.noreply.github.com>
2023-07-08 15:07:34 +0900

    Revert "Bottom navigation bar"

--- app/src/main/java/edu/kaist/cs/teamfinder/NavBarItem.kt
+++
@@ -1,9 +0,0 @@
-package edu.kaist.cs.teamfinder
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavBarItem(
-    val icon: ImageVector,
-    val name: String,
-    val route: String,
-)
--- app/src/main/java/edu/kaist/cs/teamfinder/NavRoutes.kt
+++
@@ -1,9 +0,0 @@
-package edu.kaist.cs.teamfinder
-
-sealed class NavRoutes(val route: String) {
-    object Home: NavRoutes("home")
-    object Posting: NavRoutes("posting")
-    object Add: NavRoutes("add")
-    object Chat: NavRoutes("chat")
-    object Star: NavRoutes("star")
-}

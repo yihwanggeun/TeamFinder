5cd8104cb6101145907cdf532490f6858142591e
Mo Jihun <jihunmo@kaist.ac.kr>
2023-07-08 14:25:58 +0900

    minor changes

--- app/src/main/java/edu/kaist/cs/teamfinder/ui/theme/Theme.kt
+++ app/src/main/java/edu/kaist/cs/teamfinder/ui/theme/Theme.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -20,14 +19,12 @@ private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80,
-    background = Color.Red,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-    background = Color.Red,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
